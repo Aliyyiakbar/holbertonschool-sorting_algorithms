15d834745a45e87c1127ec28a1e1b98f32410992
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-02-06 13:58:47 +0400

    Refactor cocktail_sort_list function to improve structure and readability; separate forward and backward pass logic

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -34,13 +34,70 @@ void _swap(listint_t **list, listint_t *l, listint_t *r)
 }
 
 /**
+ * fwd - Performs the forward pass of cocktail sort
+ * @list: Double pointer to the head of the list
+ * @lb: Left bound (exclusive)
+ * @rb: Right bound (exclusive)
+ * Return: 1 if swapped happened, 0 otherwise
+ */
+static int fwd(listint_t **list, listint_t *lb, listint_t **rb)
+{
+	listint_t *c;
+	int s = 0;
+
+	c = (lb) ? lb : *list;
+	while (c->next != *rb)
+	{
+		if (c->n > c->next->n)
+		{
+			_swap(list, c, c->next);
+			s = 1;
+			print_list(*list);
+		}
+		else
+		{
+			c = c->next;
+		}
+	}
+	*rb = c;
+	return (s);
+}
+
+/**
+ * bwd - Performs the backward pass of cocktail sort
+ * @list: Double pointer to the head of the list
+ * @lb: Left bound (exclusive)
+ * @rb: Right bound (exclusive)
+ * Return: New left bound after the pass
+ */
+static listint_t *bwd(listint_t **list, listint_t *lb, listint_t *rb)
+{
+	listint_t *c;
+
+	c = rb;
+	while (c->prev != lb)
+	{
+		if (c->n < c->prev->n)
+		{
+			_swap(list, c->prev, c);
+			print_list(*list);
+		}
+		else
+		{
+			c = c->prev;
+		}
+	}
+	return (c);
+}
+
+/**
  * cocktail_sort_list - Sorts a doubly linked list of integers in ascending
  *                      order using the Cocktail Shaker sort algorithm
  * @list: Double pointer to the head of the doubly linked list
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *c, *lb, *rb;
+	listint_t *lb, *rb;
 	int s;
 
 	if (!list || !*list || !(*list)->next)
@@ -50,48 +107,14 @@ void cocktail_sort_list(listint_t **list)
 
 	lb = NULL;
 	rb = NULL;
-	s = 1;
 
-	while (s)
+	while (1)
 	{
-		s = 0;
-
-		c = (lb) ? lb : *list;
-		while (c->next != rb)
-		{
-			if (c->n > c->next->n)
-			{
-				_swap(list, c, c->next);
-				s = 1;
-				print_list(*list);
-			}
-			else
-			{
-				c = c->next;
-			}
-		}
-		rb = c;
-
+		s = fwd(list, lb, &rb);
 		if (!s)
 		{
 			break;
 		}
-
-		s = 0;
-
-		while (c->prev != lb)
-		{
-			if (c->n < c->prev->n)
-			{
-				_swap(list, c->prev, c);
-				s = 1;
-				print_list(*list);
-			}
-			else
-			{
-				c = c->prev;
-			}
-		}
-		lb = c;
+		lb = bwd(list, lb, rb);
 	}
 }
